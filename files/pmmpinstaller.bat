e0c10c2758f5aad5f636c8a53e8878a71592970a
Anonymous <>
2021-01-24 15:57:03 -0800

    Initial commit

---
+++ files/pmmpinstaller.bat
@@ -0,0 +1,67 @@
+@echo off
+:startme
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+if %OS%==32BIT echo This is a 32bit operating system & goto dl32
+if %OS%==64BIT echo This is a 64bit operating system & goto dl64
+goto :EOF
+
+:dl32
+cls
+call :title
+:call :downloading
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/NotPocketMine/Windows-PocketMine-MP/blob/master/PocketMine-MP-x86.exe?raw=true', 'PocketMine-MP-x86.exe')"
+powershell -Command "Invoke-WebRequest http://www.foo.com/package.zip -OutFile package.zip"
+IF EXIST "%cd%/PocketMine-MP-x86.exe" (
+start PocketMine-MP-x86.exe
+goto downloaded
+) ELSE (
+goto failure
+)
+goto :EOF
+
+:dl64
+cls
+call :title
+call :downloading
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/NotPocketMine/Windows-PocketMine-MP/blob/master/PocketMine-MP-x64.exe?raw=true', 'PocketMine-MP-x64.exe')"
+IF EXIST "%cd%/PocketMine-MP-x64.exe" (
+start PocketMine-MP-x64.exe
+goto downloaded
+) ELSE (
+goto failure
+)
+goto :EOF
+
+:downloaded
+cls
+call :title
+echo File has downloaded. 
+echo it can now be located in "%cd%".
+echo.
+echo Press any key to exit.
+pause >nul
+exit
+goto :EOF
+
+:downloading
+Echo Downloading: Please Wait...
+goto :EOF
+
+:failure
+cls
+call :title
+echo Failed to download file.
+echo.
+echo Possible reasons for error are:
+echo   -  No Internet
+echo   -  Connection interuption
+echo   -  File does not exist.
+echo.
+echo Press any key to exit.
+pause >nul
+exit
+
+:title
+powershell Write-Host PocketMine-Download-Windows [Version 1.0 - PocketMine-MP] -foregroundcolor "GREEN"
+echo (C) 2016 Microsoft Corporation.&echo (C) 2016 PocketMine-MP - PMMP &echo.
+goto :EOF
